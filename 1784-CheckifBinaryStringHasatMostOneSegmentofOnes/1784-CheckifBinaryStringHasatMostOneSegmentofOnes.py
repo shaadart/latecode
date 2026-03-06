9f71e32ed5d25c632c20580f6e18529aad1f1e5e
Shaad <64954854+shaadart@users.noreply.github.com>
2026-03-06 12:54:36 +0530

    File updated at 3/6/2026, 12:54:35 PM

--- 1784-CheckifBinaryStringHasatMostOneSegmentofOnes/1784-CheckifBinaryStringHasatMostOneSegmentofOnes.py
+++ 1784-CheckifBinaryStringHasatMostOneSegmentofOnes/1784-CheckifBinaryStringHasatMostOneSegmentofOnes.py
@@ -1,26 +1,25 @@
-# Last updated: 3/6/2026, 12:51:57 PM
+# Last updated: 3/6/2026, 12:54:35 PM
 1class Solution:
 2    def checkOnesSegment(self, s: str) -> bool:
 3        
 4        count = 0 
 5        n = len(s)
-6        i = 0
-7
-8        while i < n:
-9
-10            if s[i] == '1':
-11                count+=1
-12                while i < n and s[i] == "1":
-13                    i+=1
-14
-15            else: 
-16                i+=1
-17
-18        
-19        if count == 1:
-20            return True
-21
-22        return False
+6        i = 0 
+7        while i < n:
+8
+9            if s[i] == '1':
+10                count+=1
+11                while i < n and s[i] == "1":
+12                    i+=1
+13
+14            else: 
+15                i+=1
+16
+17        
+18        if count == 1:
+19            return True
+20
+21        return False
+22
 23
-24
-25        
+24        
