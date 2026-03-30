39c6694cd828d2e555276bbf6a6bab6de9ed21ad
Shaad <64954854+shaadart@users.noreply.github.com>
2026-03-30 10:25:56 +0530

    Time: 0 ms (100.00%) | Memory: 151 MB (20.17%) - LeetSync

---
+++ 66-plus-one/plus-one.dart
@@ -0,0 +1,15 @@
+class Solution {
+  List<int> plusOne(List<int> digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] == 9) {
+        digits[i] = 0;
+      } else {
+        digits[i]++;
+        return digits;
+      }
+    }
+
+    digits.insert(0, 1);
+    return digits;
+  }
+}
