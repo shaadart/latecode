7f8616164b4db4668e635052dc4bfb0f92aec6c4
Shaad <64954854+shaadart@users.noreply.github.com>
2026-04-27 12:17:39 +0530

    File updated at 4/27/2026, 12:17:39 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,9 +1,30 @@
-# Last updated: 4/27/2026, 11:14:52 AM
+# Last updated: 4/27/2026, 12:17:39 PM
 1class Solution:
-2    def isAnagram(self, s: str, t: str) -> bool:
-3        s1 = "".join(sorted(s))
-4        s2 = "".join(sorted(t))
-5
-6        return s1==s2
-7
-8        
+2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+3
+4        out = []
+5        sortedstrs = strs[:]
+6
+7        #sort
+8        for i in range(len(strs)):
+9            sortedstrs[i] = "".join(sorted(strs[i]))
+10
+11        
+12        # for i in range(len(sortedstrs)):
+13        #     for j in range():
+14        #     if sortedstrs[i] == sortedstrs[i-1]:
+15        
+16        groups = defaultdict(list)
+17
+18        for i in range(len(strs)):
+19            groups[sortedstrs[i]].append(strs[i])
+20
+21
+22        for key in groups:
+23            out.append(groups[key])
+24
+25        return out
+26
+27                
+28
+29        
