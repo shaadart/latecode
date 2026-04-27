50ea65155027c9a83908b6d979359c35b04fedb4
Shaad <64954854+shaadart@users.noreply.github.com>
2026-04-27 12:50:01 +0530

    Time: 58 ms (5.09%) | Memory: 22.7 MB (95.33%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #sort
+        nums.sort()
+        out = []
+        
+        #count and store 
+        groups = {}
+
+        for i in nums:
+            if i in groups:  
+                groups[i] = groups.get(i) + 1
+            else: 
+                groups[i] = 1
+
+        print(groups)
+        #extract k times highest oness
+        while k != 0:
+            highest_key = max(groups, key=groups.get) #1
+            out.append(highest_key)
+            groups.pop(highest_key,None)      
+            k-=1
+
+        return out
