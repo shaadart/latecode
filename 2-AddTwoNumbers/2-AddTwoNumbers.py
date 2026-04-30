dea505ecff90be9e7ff95ba9d9fd13ce23855063
Shaad <64954854+shaadart@users.noreply.github.com>
2026-04-30 10:22:58 +0530

    File updated at 4/30/2026, 10:22:58 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.py
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -1,4 +1,12 @@
-# Last updated: 4/30/2026, 10:02:03 AM
+# Last updated: 4/30/2026, 10:22:58 AM
 1class Solution:
-2    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-3        return list(set(nums1) & set(nums2))
+2    def missingNumber(self, nums: List[int]) -> int:
+3        count = len(nums)
+4        nums.sort()
+5        for i in range(count):
+6
+7            if nums[i] != i:
+8                return i
+9
+10        return count
+11        
