483cca0482a40948d138de6b15bd517bd4d4d6d8
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-01 09:52:54 +0530

    File updated at 5/1/2026, 9:52:54 AM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
@@ -1,14 +1,15 @@
-# Last updated: 5/1/2026, 9:52:35 AM
+# Last updated: 5/1/2026, 9:52:54 AM
 1class Solution:
 2    def maxProfit(self, prices: List[int]) -> int:
-3        r,maxP = 1,0
-4      
-5
-6        while r < len(prices):
-7            if prices[r-1] < prices[r]:
-8                maxP += prices[r] - prices[r - 1]
-9            r+=1
-10       
-11
-12        return maxP 
-13        
+3        #this is a two pointer shit, not sliding window
+4        r,maxP = 1,0
+5      
+6
+7        while r < len(prices):
+8            if prices[r-1] < prices[r]:
+9                maxP += prices[r] - prices[r - 1]
+10            r+=1
+11       
+12
+13        return maxP 
+14        
