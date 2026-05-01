ab21b86c7894da43c12ac3e40a2f5ce5387ffdce
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-01 22:54:00 +0530

    File updated at 5/1/2026, 10:53:59 PM

--- 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.py
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.py
@@ -1,16 +1,14 @@
-# Last updated: 5/1/2026, 10:40:50 AM
+# Last updated: 5/1/2026, 10:53:59 PM
 1class Solution:
-2    def firstUniqChar(self, s: str) -> int:
-3        freq = {}
-4
-5        for ch in s:
-6            freq[ch] = freq.get(ch, 0) + 1
-7
-8          
-9
-10        for i, ch in enumerate(s):
-11            if freq[ch] == 1:
-12                return i
-13            
-14        return -1
-15        
+2    def pivotIndex(self, nums: List[int]) -> int:
+3        lsum = 0
+4        rsum = sum(nums)
+5
+6        for i in range(len(nums)):
+7            rsum -= nums[i]
+8            
+9            if lsum == rsum:
+10                return i
+11            lsum += nums[i]
+12
+13        return -1
