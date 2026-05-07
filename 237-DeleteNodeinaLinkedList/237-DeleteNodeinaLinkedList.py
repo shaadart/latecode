3f7bc40858208d6bd29cd6e156013ad7e86d03d2
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 11:05:45 +0530

    File updated at 5/7/2026, 11:05:45 AM

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.py
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.py
@@ -1,17 +1,25 @@
-# Last updated: 5/7/2026, 10:35:46 AM
+# Last updated: 5/7/2026, 11:05:45 AM
 1# Definition for singly-linked list.
 2# class ListNode:
-3#     def __init__(self, x):
-4#         self.val = x
-5#         self.next = None
-6
-7class Solution:
-8    def deleteNode(self, node):
-9        """
-10        :type node: ListNode
-11        :rtype: void Do not return anything, modify node in-place instead.
-12        """
-13
-14        node.val = node.next.val
-15        node.next = node.next.next
-16        
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8
+9        fast = head
+10
+11        if head == None or head.next == None:
+12            return head
+13     
+14
+15        while fast!=None and fast.next!=None: 
+16            if fast.next.val == fast.val:
+17                fast.next = fast.next.next
+18            else:
+19                fast = fast.next
+20
+21        return head
+22
+23
+24        
