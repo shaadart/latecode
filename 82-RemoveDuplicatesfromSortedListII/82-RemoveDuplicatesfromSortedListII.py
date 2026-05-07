1e37843bf2cec52676e286c438c280a483215d3c
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 14:29:19 +0530

    File updated at 5/7/2026, 2:29:19 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
@@ -1,13 +1,46 @@
-# Last updated: 5/7/2026, 12:45:18 PM
-1class Solution:
-2    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-3
-4        arr = []
-5
-6        curr = head
-7
-8        while curr: 
-9            arr.append(curr.val)
-10            curr = curr.next
-11
-12        return arr == arr[::-1]
+# Last updated: 5/7/2026, 2:29:19 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.next = None
+6
+7class Solution:
+8    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+9        def getlen(h):
+10            curr = h
+11            count = 0
+12            while curr:
+13                count+=1
+14                curr=curr.next
+15
+16            return count
+17
+18        lenA = getlen(headA)
+19        lenB = getlen(headB)
+20
+21        
+22        #match
+23        currB,currA = headB,headA
+24        while max(lenA, lenB)!= min(lenA,lenB):
+25            if lenB > lenA:
+26                currB = currB.next
+27                lenB-=1
+28
+29            else:
+30                currA = currA.next
+31                lenA-=1
+32
+33        
+34        #check
+35        while currA:
+36            if currA == currB:
+37                return currA
+38
+39            currA= currA.next
+40            currB= currB.next
+41
+42            
+43                
+44
+45        
