74c64ad0699a7958126f66824ce13cf186252ed3
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 23:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.20%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
@@ -2,7 +2,7 @@ class Solution:
     def replaceDigits(self, s: str) -> str:
         s = list(s)
         for i in range(1, len(s), 2):
-            s[i] = chr(ord(s[i-1])+ int(s[i]))
+            s[i] = chr(ord(s[i-1])+ int(s[i])) #i-1 is because we are checking "a" i.e before number
 
         return "".join(s)
         
