543a0b30c7863c078bf79372525253971dcd8918
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 23:15:13 +0530

    File updated at 5/7/2026, 11:15:13 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -1,9 +1,9 @@
-# Last updated: 5/7/2026, 11:14:53 PM
+# Last updated: 5/7/2026, 11:15:13 PM
 1class Solution:
 2    def replaceDigits(self, s: str) -> str:
 3        s = list(s)
 4        for i in range(1, len(s), 2):
-5            s[i] = chr(ord(s[i-1])+ int(s[i]))
+5            s[i] = chr(ord(s[i-1])+ int(s[i])) #i-1 is because we are checking "a" i.e before number
 6
 7        return "".join(s)
 8        
