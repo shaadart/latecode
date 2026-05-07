2764247e5dc0e6b64ec8664a6dba14daeef20478
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 12:45:19 +0530

    File updated at 5/7/2026, 12:45:18 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
@@ -1,22 +1,13 @@
-# Last updated: 5/7/2026, 12:39:15 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8       
-9        curr = head
-10        prev = None
-11        nxt = None
-12
-13        while curr!= None:
-14            nxt = curr.next
-15            curr.next = prev
-16            prev = curr
-17            curr = nxt
-18
-19
-20        return prev
-21        
+# Last updated: 5/7/2026, 12:45:18 PM
+1class Solution:
+2    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+3
+4        arr = []
+5
+6        curr = head
+7
+8        while curr: 
+9            arr.append(curr.val)
+10            curr = curr.next
+11
+12        return arr == arr[::-1]
