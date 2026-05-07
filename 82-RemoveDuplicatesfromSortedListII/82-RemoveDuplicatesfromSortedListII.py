58f7054f31cf4b960ff3296e1cd56d9079c9eb43
Shaad <64954854+shaadart@users.noreply.github.com>
2026-05-07 22:46:54 +0530

    File updated at 5/7/2026, 10:46:54 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.py
@@ -1,13 +1,18 @@
-# Last updated: 5/7/2026, 9:02:54 PM
+# Last updated: 5/7/2026, 10:46:54 PM
 1class Solution:
-2    def findDuplicate(self, nums: List[int]) -> int:
-3
-4        #store in dick
-5        freq = {}
-6        n = len(nums)
-7        for i in range(n):
-8            # if freq[nums[i]].get()
-9             freq[nums[i]] = freq.get(nums[i], 0) + 1
+2    def isAnagram(self, s: str, t: str) -> bool:
+3        freqs = {}
+4        freqt= {}
+5
+6        for i in range(len(s)):
+7            # print(freqs[s[i]])
+8            # print(freqs[s[i]])
+9            freqs[s[i]] = freqs.get(s[i], 0) + 1
 10
-11        return max(freq, key=freq.get)
-12        
+11        for i in range(len(t)):
+12            freqt[t[i]] = freqt.get(t[i], 0) + 1
+13            
+14        return freqs == freqt
+15
+16
+17        
