bed60c61c71bd04b0a85885eed8457d2ae8dc965
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-06-13 11:40:38 +0530

    File updated at 6/13/2026, 11:40:38 AM

--- 189-RotateArray/189-RotateArray.py
+++ 189-RotateArray/189-RotateArray.py
@@ -1,13 +1,12 @@
-# Last updated: 6/13/2026, 11:04:20 AM
+# Last updated: 6/13/2026, 11:40:38 AM
 1class Solution:
 2    def rotate(self, nums: List[int], k: int) -> None:
-3        """
-4        Do not return anything, modify nums in-place instead.
-5        """
-6
-7        for i in range(k):
-8            rem = nums.pop()
-9            nums.insert(0, rem)
-10
-11        return nums
-12        
+3        
+4
+5        n = len(nums)
+6        k %= n 
+7        l = nums[:n-k]
+8        r = nums[n-k:]
+9        nums[:] = r+l
+10            
+11        
