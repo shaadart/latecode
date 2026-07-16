bffa130610b8df2e57cfa455ebc37bd3c8b7ce32
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-07-17 00:02:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.py
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.py
@@ -1,5 +1,8 @@
 class Solution:
     def subarraySum(self, arr, target):
+        if not arr:
+            return [-1]
+            
         n = len(arr)
         i = 0
         j = 0
@@ -7,20 +10,56 @@ class Solution:
 
         while j < n:
             if tot == target:
-                return [i + 1, j + 1]
-
-            elif tot < target:
+                return [i+1, j+1]
+                
+            elif tot > target: 
+                
+                tot -= arr[i]
+                i += 1
+        
+                if i < n and i > j: 
+                    j = i
+                    tot = arr[i]
+                    
+            else: 
                 j += 1
                 if j == n:
                     break
                 tot += arr[j]
-
-            else:
-                tot -= arr[i]
-                i += 1
-
-                if i > j and i < n:
-                    j = i
-                    tot = arr[i]
-
+                
+                
         return [-1]
+                
+                
+                
+    #             class Solution:
+    # def subarraySum(self, arr, target):
+    #     n = len(arr)
+    #     i = 0
+    #     j = 0
+    #     tot = arr[0]
+
+    #     while j < n:
+    #         if tot == target:
+    #             return [i + 1, j + 1]
+
+    #         elif tot < target:
+    #             j += 1
+    #             if j == n:
+    #                 break
+    #             tot += arr[j]
+
+    #         else:
+    #             tot -= arr[i]
+    #             i += 1
+
+    #             if i > j and i < n:
+    #                 j = i
+    #                 tot = arr[i]
+
+    #     return [-1]
+                
+                
+                
+                
+                
