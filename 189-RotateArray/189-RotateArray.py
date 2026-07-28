1b7c0e3c7f17fed6090a5e13888709c8843fc398
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-07-28 12:41:22 +0530

    File updated at 28/07/2026, 12:41:22

--- 189-RotateArray/189-RotateArray.py
+++ 189-RotateArray/189-RotateArray.py
@@ -1,12 +1,10 @@
-# Last updated: 6/13/2026, 11:45:28 AM
+# Last updated: 28/07/2026, 12:41:22
 1class Solution:
 2    def rotate(self, nums: List[int], k: int) -> None:
-3        
-4
-5        n = len(nums)
-6        k %= n 
-7        r = nums[:n-k]
-8        l = nums[n-k:]
-9        nums[:] = l+r
-10            
-11        
+3        n = len(nums)
+4        k %= n
+5
+6        r = nums[:n-k]
+7        l = nums[n-k:]
+8        nums[:] = l + r
+9
