224870780b1794abf9d17f72900922e1bf409be7
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-07-29 10:08:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.py
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.py
@@ -1,16 +1,5 @@
-'''
-Definition for Node
-class Node:
-    def _init_(self, val):
-        self.data = val
-        self.left = None
-        self.right = None
-'''
-
 class Solution:
     def height(self, root):
-        if root is None: 
-            return -1
-            
-        return 1+max(self.height(root.left), self.height(root.right))
-    
+        if root is None:
+            return -1  # Returns height based on edges rather than nodes
+        return 1 + max(self.height(root.left), self.height(root.right))
