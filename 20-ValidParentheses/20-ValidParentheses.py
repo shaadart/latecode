27d1a20a3e02f614cf15786703b9cd24f587155c
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-08-05 12:39:26 +0530

    File updated at 05/08/2026, 12:39:26

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,20 +1,18 @@
-# Last updated: 05/08/2026, 12:08:57
+# Last updated: 05/08/2026, 12:39:26
 1class Solution:
-2    def isValid(self, s: str) -> bool:
-3        #defination
-4        stack = []
-5        bracketmap = {")": "(", "}": "{", "]": "["}
+2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+3        intervals.sort()
+4
+5        answer = [intervals[0]]
 6
-7        for ch in s:
-8            if ch in "([{":
-9                stack.append(ch)
+7        for current in intervals[1:]:
+8
+9            previous = answer[-1]
 10
-11            else:
-12                if not stack:
-13                    return False
-14
-15                if stack.pop() != bracketmap[ch]:
-16                    return False
-17
-18
-19        return len(stack) == 0
+11            if current[0] <= previous[1]:
+12                previous[1] = max(previous[1], current[1])
+13
+14            else:
+15                answer.append(current)
+16
+17        return answer
