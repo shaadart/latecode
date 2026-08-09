ac6423c8dd8231086885cec81f0627b79208f65f
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-08-09 12:02:58 +0530

    File updated at 09/08/2026, 12:02:58

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,34 +1,29 @@
-# Last updated: 09/08/2026, 11:44:45
+# Last updated: 09/08/2026, 12:02:58
 1class Solution:
 2    def longestPalindrome(self, s: str) -> str:
 3        n = len(s)
 4        res = ""
 5        reslen = 0
-6        for i in range(n):
-7            #odd length
-8            l,r = i , i
-9            while l>=0 and r<n and s[l] == s[r]:
-10                if r-l+1 > reslen:
-11                    res = s[l:r+1]
-12                    reslen = r-l+1
-13                l-=1
-14                r+=1
-15
-16            #even length
-17            l,r = i,i+1
-18            while l>=0 and r<n and s[l] == s[r]:
-19                if r-l+1>reslen:
-20                    res = s[l:r+1]
-21                    reslen = r - l +1
-22
-23                l-=1
-24                r+=1
-25
-26            
-27        return res
-28
-29
-30
-31
-32
-33        
+6
+7        for i in range(n):
+8            #odd length
+9            l,r, = i,i
+10            while l>=0 and r < n and s[l] == s[r]:
+11                if reslen < (r - l + 1):
+12                    res = s[l:r+1]
+13                    reslen = r - l + 1 
+14
+15                l-=1
+16                r+=1
+17
+18            #even length
+19            l,r = i, i+1
+20            while l>=0 and r<n and s[l] == s[r]:
+21                if reslen < (r-l+1):
+22                    res = s[l:r+1]
+23                    reslen = r-l+1
+24
+25                l-=1
+26                r+=1
+27
+28        return res
