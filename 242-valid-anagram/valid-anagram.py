06223a826cca3a907011fb0679a07f41bf01ba09
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-08-21 07:46:03 +0530

    Time: 7 ms (91.65%) | Memory: 19.3 MB (76.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freqs = {}
-        freqt= {}
+        seeni = {}
+        seenj = {}
 
-        for i in range(len(s)):
-            # print(freqs[s[i]])
-            # print(freqs[s[i]])
-            freqs[s[i]] = freqs.get(s[i], 0) + 1
+        for i in s: 
+            seeni[i] = seeni.get(i, 0)+1
 
-        for i in range(len(t)):
-            freqt[t[i]] = freqt.get(t[i], 0) + 1
-            
-        return freqs == freqt
+        for j in t: 
+            seenj[j] = seenj.get(j, 0)+1
 
+        return seenj == seeni
+
+        
 
         
