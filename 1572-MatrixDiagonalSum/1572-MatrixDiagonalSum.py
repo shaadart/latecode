5ce3fd752386a01ef9ca0e6582c573949e83927d
Shaad Ashraf <64954854+shaadart@users.noreply.github.com>
2026-08-21 07:32:51 +0530

    File updated at 21/08/2026, 07:32:50

--- 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.py
+++ 1572-MatrixDiagonalSum/1572-MatrixDiagonalSum.py
@@ -1,47 +1,18 @@
-# Last updated: 21/08/2026, 07:15:03
+# Last updated: 21/08/2026, 07:32:50
 1class Solution:
 2    def diagonalSum(self, mat: List[List[int]]) -> int:
 3        n = len(mat)
-4        out = []
+4        total=0
 5
-6        if n % 2 == 0:
-7            for i in range(n):
+6        for i in range(n):
+7            total += mat[i][i]
 8
-9                for j in range(n):
+9            total += mat[i][n-1-i]
 10
-11                    if i == j:
-12                        out.append(mat[i][j])
-13
-14            r, c = 0, n - 1
-15            while c >= 0 and r < n:
-16                out.append(mat[r][c])
+11        if n%2!=0:
+12            mid = n // 2
+13            total-= mat[mid][mid]
+14
+15
+16        return total
 17
-18                r += 1
-19                c -= 1
-20
-21        else:
-22            for i in range(n):
-23
-24                for j in range(n):
-25
-26                    if i == j:
-27                        out.append(mat[i][j])
-28
-29            r, c = 0, n - 1
-30
-31            while c >= 0 and r < n:
-32                if r == c:
-33                    r += 1
-34                    c -= 1
-35                    continue  
-36
-37                out.append(mat[r][c])
-38                
-39                r += 1
-40                c -= 1
-41
-42
-43            
-44
-45        return sum(out)
-46
